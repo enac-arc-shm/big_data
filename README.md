cb520e813234669870c4b24d8aff1ff14ee183dd
enac-arc-shm <21300018@uttt.edu.mx>
2023-01-15 16:58:34 -0600

    enero 15

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # big_data
+Excercices of python pandas data science 
+Dependencies 
+- pip install pandas 
+- pip install matplotlib
+- pip install python3 -m 
+- pip install reportlab
--- ejercicio1.py
+++ ejercicio1.py
@@ -1,6 +1,13 @@
 import pandas as pd
-import matplotlib.pyplot as pld
+import matplotlib.pyplot as plt
+from reportlab.lib.pagesizes import letter
+from reportlab.pdfgen import canvas
 
+
+def add_image(image_path):
+    my_canvas = canvas.Canvas("canvas_image.pdf", pagesize=letter)
+    my_canvas.drawImage(image_path, 30, 600, width=100, height=100)
+    my_canvas.save()
 
 def recopilar_datos():
     fuente_datos = input("Ingresa la ruta del archivo: ")
@@ -30,14 +37,25 @@ def modelo_analisis(df):
     malware = df.query("type == 'malware'")
     total_malware = malware.count()
     lista_resultados +=  ["malware", total_malware ]
-    presentar_datos(df,lista_resultados)
+    grafica_barras(df,lista_resultados)
+    grafica_circular(df,lista_resultados)
     
-def presentar_datos(df, resultados):
+def grafica_barras(df, resultados):
     valor_x = df['type'].unique()
     valor_y = df['type'].value_counts().tolist()
-    pld.bar(valor_x, valor_y)
-    pld.show()
-    pld.close('all')
+    plt.bar(valor_x, valor_y)
+    plt.savefig("Data/Img/figura1.png")
+    plt.close('all')
+
+def grafica_circular(df, resultados):
+    fig, ax = plt.subplots()
+    ax.pie(df['type'].value_counts().tolist())
+    plt.show()
+
+def generar_informe():
+    name = input("Ingrese el nombre del informe: ") 
 
 if __name__ == '__main__':
     recopilar_datos()
+    #image_path = 'snakehead.jpg'
+    #add_image(image_path)
